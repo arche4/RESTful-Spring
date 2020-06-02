994b964c30bd16773cc9fce2ef5652e0d9d5d819
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-06-01 20:08:37 -0500

    Creacion proyecto

---
+++ src/main/java/co/com/poli/servicesproduct/ServicesProductApplication.java
@@ -0,0 +1,13 @@
+package co.com.poli.servicesproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/poli/servicesproduct/ServicesProductApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.servicesproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
